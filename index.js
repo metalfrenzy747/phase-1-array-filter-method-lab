1be0d95e872820582967469453bd03295bfe4018
metalfrenzy747 <allenjeremy747@gmail.com>
2024-01-15 12:38:03 -0500

    array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter((el) => el.toLowerCase().includes(name.toLowerCase()));
+}
+function fuzzyMatch(drivers, element) {
+    return drivers.filter(driver => driver.slice(0, element.length) === element);
+}
+function matchName(drivers, name) {
+    const matchingDrivers= []
+    for (const driver of drivers) {
+        if (driver.name === name) {
+            matchingDrivers.push(driver);
+        }
+    }
+    return matchingDrivers;
+}
